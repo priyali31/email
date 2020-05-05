19db41617c97f72c1890e3b086796573ab7efd15
priyali sakhare <priyalisakhare31@gmail.com>
2020-05-05 16:45:08 +0530

    added email.sh

---
+++ email.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+while true
+do
+    read -p "Enter email: " email
+    echo
+    if [[ "$email" =~ ^[a-zA-Z0-9+]+$ ]]
+    then
+        echo "Email $email is valid."
+        break
+    else
+        echo "Email $email is invalid."
+    fi
+done
